72e8f70f8fedd02bc047a34d6a96b0409c5d4286
Valentin Secades <vsecades@qxdev.com>
2020-11-30 10:51:51 -0600

    Update to sample for plugin refactor and base tests.

--- example/lib/liveTests.dart
+++ example/lib/liveTests.dart
@@ -1,25 +1,27 @@
 import 'package:veridflutterplugin/veridflutterplugin.dart';
+import 'package:veridflutterplugin/src/VerID.dart';
 import 'dart:async';
 import 'dart:developer' as developer;
 import 'package:flutter/services.dart';
 
 class LiveTests {
   // Todo add more test functions
-  static void registerUser(String userId) {
-  }
+  static void registerUser(String userId) {}
 
-  static Future<String> load() async {
+  static Future<VerID> load() async {
     String pluginProcessResult;
-    Map<String, dynamic> result;
+    VerID verID;
+    ;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       //platformVersion = await Veridflutterplugin.platformVersion;
-      result = await Veridflutterplugin.load(null); //.load('efe89f85-b71f-422b-a068-605c3f62603b');
-      pluginProcessResult = result.toString();
+      verID = await Veridflutterplugin
+          .load(); //.load('efe89f85-b71f-422b-a068-605c3f62603b');
+      pluginProcessResult = "";
     } on PlatformException catch (ex) {
       pluginProcessResult = 'Uncaught Platform Exception';
       developer.log(ex.message.toString());
     }
-    return pluginProcessResult;
+    return verID;
   }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:veridflutterplugin_example/liveTests.dart';
+import 'dart:developer' as developer;
+import 'package:flutter/services.dart';
+import 'package:veridflutterplugin/veridflutterplugin.dart';
+import 'package:veridflutterplugin/src/VerID.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,31 +26,55 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    String pluginProcessResult;
+    VerID result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      //platformVersion = await Veridflutterplugin.platformVersion;
+      result = await Veridflutterplugin
+          .load(); //.load('efe89f85-b71f-422b-a068-605c3f62603b');
+      pluginProcessResult = "VerID instance loaded successfully.";
+    } on PlatformException catch (ex) {
+      pluginProcessResult = 'Platform Exception: ' + ex.message.toString();
+      developer.log(ex.message.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
     if (!mounted) return;
   }
 
   Future<void> load() async {
-    String result = await LiveTests.load();
-    setState(() {
+    VerID result = await LiveTests.load();
+    /*setState(() {
       _loadResult = result;
-    });
+    });*/
   }
 
   List<Widget> getButtonList() {
     List<RaisedButton> buttons = [];
-    buttons.add(createButton('load', () {load();}));
-    buttons.add(createButton('Register User', () {LiveTests.registerUser('userId');}));
+    buttons.add(createButton('load', () {
+      load();
+    }));
+    buttons.add(createButton('Register User', () {
+      LiveTests.registerUser('userId');
+    }));
     return buttons;
   }
 
   Widget createButton(String name, Function callback) {
-    return RaisedButton(onPressed: callback, child: Text(name), color: Colors.blue, textColor: Colors.white);
+    return RaisedButton(
+        onPressed: callback,
+        child: Text(name),
+        color: Colors.blue,
+        textColor: Colors.white);
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> content = [ Center(child: Text('Load result: $_loadResult\n'))];
-    content.addAll(getButtonList().map((Widget button){
+    List<Widget> content = [Center(child: Text('Load result: $_loadResult\n'))];
+    content.addAll(getButtonList().map((Widget button) {
       return button;
     }).toList());
 
@@ -56,12 +84,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Ver-ID plugin example app'),
         ),
         body: Center(
-          //child: Text('Running on: $_platformVersion\n' ),
-          child:ListView(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 50),
-            children: content,
-          )
-        ),
+            //child: Text('Running on: $_platformVersion\n' ),
+            child: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 50),
+          children: content,
+        )),
       ),
     );
   }
