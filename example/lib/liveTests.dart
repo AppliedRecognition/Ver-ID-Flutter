b80563388c437d06be3bc2878cae7c4ef512f0b3
Valentin Secades <vsecades@qxdev.com>
2020-12-21 15:34:34 -0600

    Updating unit testing.

--- example/lib/liveTests.dart
+++ example/lib/liveTests.dart
@@ -1,7 +1,6 @@
 import 'package:veridflutterplugin/veridflutterplugin.dart';
 import 'package:veridflutterplugin/src/VerID.dart';
 import 'dart:async';
-import 'dart:developer' as developer;
 import 'package:veridflutterplugin/src/RegistrationSessionSettings.dart';
 import 'package:veridflutterplugin/src/AuthenticationSessionSettings.dart';
 import 'package:veridflutterplugin/src/LivenessDetectionSessionSettings.dart';
@@ -15,10 +14,11 @@ class LiveTests {
   // Todo add more test functions
   Future<SessionResult> registerUser(String userId) async {
     if (verID != null) {
-      RegistrationSessionSettings settings = new RegistrationSessionSettings(userId: userId);
+      RegistrationSessionSettings settings =
+          new RegistrationSessionSettings(userId: userId);
       settings.bearingsToRegister = [Bearing.RIGHT];
       settings.showResult = true;
-      return verID.register(settings: settings).then((value) {
+      return VerID.register(settings: settings).then((value) {
         if (value == null) {
           throw 'Session canceled';
         }
@@ -31,8 +31,9 @@ class LiveTests {
 
   Future<SessionResult> authenticate(String userId) async {
     if (verID != null) {
-      AuthenticationSessionSettings settings = new AuthenticationSessionSettings(userId: userId);
-      return verID.authenticate(settings: settings).then((value) {
+      AuthenticationSessionSettings settings =
+          new AuthenticationSessionSettings(userId: userId);
+      return VerID.authenticate(settings: settings).then((value) {
         if (value == null) {
           throw 'Session canceled';
         }
@@ -42,10 +43,12 @@ class LiveTests {
       throw verIdNotInitialized;
     }
   }
+
   Future<SessionResult> captureLiveFace() async {
     if (verID != null) {
-      LivenessDetectionSessionSettings settings = new LivenessDetectionSessionSettings();
-      return verID.captureLiveFace(settings: settings).then((value) {
+      LivenessDetectionSessionSettings settings =
+          new LivenessDetectionSessionSettings();
+      return VerID.captureLiveFace(settings: settings).then((value) {
         if (value == null) {
           throw 'Session canceled';
         }
@@ -55,23 +58,25 @@ class LiveTests {
       throw verIdNotInitialized;
     }
   }
+
   /**
    * Get a list of users with registered faces
    */
   Future<List<String>> getRegisteredUsers() async {
     if (verID != null) {
-      return verID.getRegisteredUsers();
+      return VerID.getRegisteredUsers();
     } else {
       throw verIdNotInitialized;
     }
   }
+
   /**
    * Delete user with registered faces
    * @param userId ID of the user to delete
    */
   Future<String> deleteRegisteredUser(String userId) async {
     if (verID != null) {
-      return verID.deleteRegisteredUser(userId);
+      return VerID.deleteRegisteredUser(userId);
     } else {
       throw verIdNotInitialized;
     }
@@ -79,9 +84,7 @@ class LiveTests {
 
   Future<VerID> load() {
     // Platform messages may fail, so we use a try/catch PlatformException.
-    return Veridflutterplugin
-        .load().then((instance) =>
-            verID = instance
-        );//.load('efe89f85-b71f-422b-a068-605c3f62603b');
+    return Veridflutterplugin.load().then((instance) =>
+        verID = instance); //.load('efe89f85-b71f-422b-a068-605c3f62603b');
   }
 }
