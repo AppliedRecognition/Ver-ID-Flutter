4a50ade6ff21cb5ec445defe4fcac9502fcaab95
Md Jihanur Rahman <64974221+jihanurrahman33@users.noreply.github.com>
2025-09-04 00:38:58 +0600

    fix(example): migrate deprecated Material buttons to modern widgets (#16)
    Co-authored-by: jihanurrahman33 <jihanurrahman33@users.noreply.github.com>

--- example/lib/uiUtils.dart
+++ example/lib/uiUtils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class UiUtils {
   static Widget createButton(String name, Function callback) {
-    return RaisedButton(
+    return ElevatedButton(
         onPressed: callback,
         child: Text(name),
         color: Colors.blue,
